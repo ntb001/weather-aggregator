bdbe6a1b716b120df81e5d423aa16ce86b98eba6
Neil Bajorin <ntb001@gmail.com>
2024-02-08 13:39:06 -0500

    remove debug in redisclient

--- clients/redisclient.pl
+++ clients/redisclient.pl
@@ -7,12 +7,6 @@ sub getRedis {
     my ($key) = @_;
     my $redis = Redis->new;
     my $value = $redis->get($key);
-    if ($value) {
-        print "Redis get $key: $value\n";
-    }
-    else {
-        print "Redis miss $key\n";
-    }
     return $value;
 }
 
@@ -20,14 +14,12 @@ sub setRedis {
     my ( $key, $value ) = @_;
     my $redis = Redis->new;
     $redis->set( $key, $value );
-    print "Redis set $key: $value\n";
 }
 
 sub setRedisTtl {
     my ( $key, $value ) = @_;
     my $redis = Redis->new;
     $redis->setex( $key, 3600, $value );
-    print "Redis set $key: $value for 1 hour\n";
 }
 
 1;
